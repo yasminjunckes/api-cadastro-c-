c014b9042b0f7f444470fc0ffa041510ea126f41
Patrícia Selpa <patricia.selpa@hbsis.com.br>
2021-06-09 15:06:27 -0300

    Criadas referências entre Domain, Infra e Web.

---
+++ Domain/Entities/Adress.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain
+{
+    public class Adress : Entity
+    {
+        public string Line1 { get; protected set; } 
+        public string Line2 { get; protected set; } 
+        public int Number { get; protected set; } //obrigatorio
+        public string PostalCode { get; protected set; } //obrigatorio
+        public string City { get; protected set; } 
+        public string District {get; protected set; }
+        public bool Principal {get; protected set; } // obrigatorio (validar se apenas um é o principal)
+
+        public Adress(string line1, string line2, int number, string postalCode, string city, string district, bool principal)
+        {
+            Id = Guid.NewGuid();
+            Line1 = line2;
+            Line2 = line2;
+            Number = number;
+            PostalCode = postalCode;
+            City = city;
+            District = district;
+            Principal = principal;
+        }
+    }
+}
